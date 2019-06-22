de36497954275110ad92de4cebc555ad22a53055
Andrew Davisson <akdavisson4@gmail.com>
2019-06-22 18:53:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,6 +55,16 @@ class Student
     student
   end
   
+  def self.find_by_name(name)
+    sql <<-SQL
+      SELECT * FROM students WHERE name = ?
+      SQL
+      
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
   def update
     sql = <<-SQL
       UPDATE students SET name = ?, grade = ? WHERE id = ?
